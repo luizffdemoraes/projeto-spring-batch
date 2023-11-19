85f3d63b9a1c7542f3d1ddce6d0d1c1bdc26e76f
Luiz Felipe <lffm1994@gmail.com>
2023-11-19 02:26:08 -0300

    Atualização para versão 5 do spring batch

--- primeiro-projeto-spring-batch/src/main/java/com/udemy/primeiroprojetospringbatch/BatchConfig.java
+++ primeiro-projeto-spring-batch/src/main/java/com/udemy/primeiroprojetospringbatch/BatchConfig.java
@@ -3,47 +3,40 @@ package com.udemy.primeiroprojetospringbatch;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
 
-@EnableBatchProcessing
+
 @Configuration
 public class BatchConfig {
 	
-	@Autowired
-	private JobBuilderFactory jobBuilderFactory;
-	
-	@Autowired
-	private StepBuilderFactory stepBuilderFactory;
-	
 	@Bean
-	public Job ImprimeOlaJob() {
-		return jobBuilderFactory
-		.get("ImprimeOlaJob")
-		.start(ImprimeOlaStep())
-		.build();
-	}
-	
-	@Bean
-	public Step ImprimeOlaStep() {
-		return stepBuilderFactory
-				.get("ImprimeOlaStep")
-				.tasklet(new Tasklet() {
-					
-					@Override
-					public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-						System.out.println();
-						return RepeatStatus.FINISHED;
-					}
-				})
+	public Job job(JobRepository jobRepository, Step step ) {
+		return new JobBuilder("job", jobRepository)
+				.start(step)
 				.build();
 	}
+	
+	@Bean
+	public Step step(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+		return (Step) new StepBuilder("step", jobRepository)
+				.tasklet(new Tasklet() {
 
+					@Override
+					public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext)
+							throws Exception {
+						System.out.println("Olá, mundo!");
+						return RepeatStatus.FINISHED;
+					}
+				}, transactionManager)
+				.build();
+				
+	}
 }
